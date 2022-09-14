aa96f47b482486c935da3c2775282f5df74a7a50
Amaechi McMillan <amaechimcmillan@gmail.com>
2022-09-14 14:12:09 -0500

    perparing for merge

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,41 @@
 let searchBar = document.querySelector("#searchBar");
-let movieSearch = document.querySelector("#movieSearch");
+let searchButton = document.querySelector("#searchButton");
+let movieReview = document.querySelector("#movieReview")
 
 
 
-
-
-
-
-
-
-
-
-
-// fetch('https://imdb-api.com/API/Reviews/k_dl1tf84m/tt1375666')
-// .then((response) => response.json())
-// .then((data) => console.log(data));
-movieSearch.addEventListener("click", function(){
+searchButton.addEventListener("click", function(){
     console.log(searchBar.value);
     
     fetch('https://imdb-api.com/en/API/SearchMovie/k_dl1tf84m/' + searchBar.value)
     .then((response) => response.json())
     .then((data) => console.log(data));
-
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// .then(function(searchData){
+//     let searchList = searchData.movie.title;
+//     console.log(searchList);
+
+// .then(function(movieReview){
+//     fetch('https://imdb-api.com/API/Reviews/k_dl1tf84m/tt1375666/' + movieReview.value)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+    
+// })
