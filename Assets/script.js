6ebcf5c306d26adbb0f57312360606cff3e08f1b
Dondre' Peltier <dondrepeltier@Dondres-Mini.attlocal.net>
2022-09-14 14:29:59 -0500

    worked on cast

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,23 @@
+fetch('https://imdb-api.com/API/FullCast/k_usqngafa/tt0110413')
+.then(response => response.json())
+.then(function(castData){
+   console.log(castData)
+   console.log(castList)
+   let castList = castData.actors;
+
+  for (let i = 0; i < castList.length; i++) {
+      const actorList = castList[i];
+      console.log(actorList.name)
+  }
+  })  
+
+
+
+
+
+
+
+
 
    
 
@@ -9,23 +29,3 @@
  
  
  
- 
- 
- 
- 
- 
- 
- 
- 
- fetch('https://imdb-api.com/API/FullCast/k_usqngafa/tt0110413')
- .then(response => response.json())
- .then(function(castData){
-     console.log(castData)
-     console.log(castList)
-     let castList = castData.actors;
-
-    for (let i = 0; i < castList.length; i++) {
-        const actorList = castList[i];
-        console.log(actorList.name)
-    }
-    })  
