2f85e6a9e1baf67bec752706abc7c101f7d756ba
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-15 10:37:56 -0500

    doing a pull

--- Assets/script.js
+++ Assets/script.js
@@ -22,3 +22,16 @@ fetch ("https://imdb-api.com/en/API/Trailer/k_r35hmdo3/tt0110413")
    document.querySelector("#trailer").href = trailerVideo;
 
 })
+
+fetch ("https://api.watchmode.com/v1/title/tt0110413/sources/?apiKey=RnlyvnUUMGtLIR0G4HawMh6rI5tprrcGonEYHw4c")
+.then(function (response) {
+
+    return response.json();
+
+})
+
+.then(function(streamingData){
+
+    console.log(streamingData);
+
+})
