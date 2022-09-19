2599effa9cacf989d53d5b16866bab157a87ecde
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-19 14:42:32 -0500

    fixed recently searched buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,9 @@
 let searchBar = document.querySelector("#searchBar");
 let searchButton = document.querySelector("#searchButton");
 let movieReview = document.querySelector("#movieReview");
-const imdbKey = 'k_r35hmdo3';
+// const imdbKey = 'k_r35hmdo3';
+const imdbKey = 'k_dl1tf84m';
+
 let imdbId; 
 let streamingDataEl = document.querySelector("#streamingData");
 console.log(streamingDataEl);
@@ -19,17 +21,11 @@ let recentlySearched = document.querySelector("#recentlySearched");
 
 searchButton.addEventListener("click", function(){
     
-    if (castEl != ""){
-        castEl.textContent = ""
-    }
 
-    if (streamingDataEl != ""){
-        streamingDataEl.textContent = ""
-    }
-
-    if (reviewEl != ""){
-        reviewEl.textContent = ""
-    }
+        castEl.innerHTML = ""
+        streamingDataEl.innerHTML ="";
+        reviewEl.innerHTML = ""
+    
     
     console.log(searchBar.value);
     movieList.push(searchBar.value);
@@ -37,7 +33,13 @@ searchButton.addEventListener("click", function(){
     localStorage.setItem("movie", JSON.stringify(movieList));
     
     // SEARCH MOVIE AND REVIEW
-    fetch(`https://imdb-api.com/en/API/SearchMovie/${imdbKey}/` + searchBar.value)
+  
+    getIMDB_ID_ByTitle(searchBar.value)
+    
+})
+
+function getIMDB_ID_ByTitle(title){
+    fetch(`https://imdb-api.com/en/API/SearchMovie/${imdbKey}/` + title)
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
@@ -45,100 +47,100 @@ searchButton.addEventListener("click", function(){
         imdbId = data.results[0].id;
         getMovieData(imdbId)
     })
-    
-    function getMovieData(imdbId){
-        fetch (`https://api.watchmode.com/v1/title/${imdbId}/sources/?apiKey=${watchmodeKey}`)
-        .then(function (response) {
-            
-            return response.json();
-            
-        })
-        
-        .then(function(streamingData){
-            let tempArray = [];
-            for (let i = 0; i < streamingData.length; i++) {
-                let streamingList = streamingData[i].name + ": " + streamingData[i].type;
-                if (!tempArray.includes(streamingList)){
-                    tempArray.push(streamingList)
-                    console.log(streamingList); 
-                    let streamingSource = document.createElement("li");
-                    streamingSource.textContent = streamingList;
-                    console.log(streamingSource);
-                    streamingDataEl.append(streamingSource);
-                }
-            }
-            
-            //    tt1375666/
-        })
-        
-        // TRAILER FETCH
-        // need to make sure this is plugged into the input
-        fetch (`https://imdb-api.com/en/API/Trailer/${imdbKey}/${imdbId}`)
-        .then(function (response) {
-            
-            return response.json();
-            
-        })
-        
-        .then(function(trailerData){
-            
-            console.log(trailerData); 
-            let trailerVideo = trailerData.link;
-            let linkArray = trailerVideo.split("")
-            console.log(linkArray, "after split")
-            // linkArray.splice(27, 20);
-            // console.log(linkArray, "after splice")
-            trailerVideo = linkArray.join("");
-            console.log(trailerVideo);
-            document.querySelector("#trailer").href = trailerVideo;
-            
-        })
-        
-        
-        fetch(`https://imdb-api.com/API/FullCast/${imdbKey}/${imdbId}`)
-        .then(response => response.json())
-        .then(function(castData){
-            let castList = castData.actors;
-            for (let i = 0; i < 10; i++) {
-                const actorList = castList[i];
-                console.log(actorList.name);
-                let actorName = actorList.name;
-                let castMember = document.createElement("li");
-                castMember.textContent = actorName;
-                castEl.append(castMember);
-            }
-        })
-        
-        fetch(`https://imdb-api.com/API/Reviews/${imdbKey}/${imdbId}/`)
-        .then(response => response.json())
-        .then(function(reviewData){
-            console.log(reviewData)
-            let reviewList = reviewData.items;
-            for (let i = 0; i < 2; i++) {
-                const itemsList = reviewList[i];
-                console.log(itemsList.content);
-                let reviewItems = itemsList.content;
-                let movieReview = document.createElement("li");
-                movieReview.textContent = reviewItems;
-                reviewEl.append(movieReview);
-                
-                
-                
-                
-            }
-            
-            searchBar.value = "";
-        })
-        
-    }
-    
-})
 
+}
+
+function getMovieData(imdbId){
+    fetch (`https://api.watchmode.com/v1/title/${imdbId}/sources/?apiKey=${watchmodeKey}`)
+    .then(function (response) {
+        
+        return response.json();
+        
+    })
+    
+    .then(function(streamingData){
+        let tempArray = [];
+        for (let i = 0; i < streamingData.length; i++) {
+            let streamingList = streamingData[i].name + ": " + streamingData[i].type;
+            if (!tempArray.includes(streamingList)){
+                tempArray.push(streamingList)
+                console.log(streamingList); 
+                let streamingSource = document.createElement("li");
+                streamingSource.textContent = streamingList;
+                console.log(streamingSource);
+                streamingDataEl.append(streamingSource);
+            }
+        }
+        
+        //    tt1375666/
+    })
+    
+    // TRAILER FETCH
+    // need to make sure this is plugged into the input
+    fetch (`https://imdb-api.com/en/API/Trailer/${imdbKey}/${imdbId}`)
+    .then(function (response) {
+        
+        return response.json();
+        
+    })
+    
+    .then(function(trailerData){
+        
+        console.log(trailerData); 
+        let trailerVideo = trailerData.link;
+        let linkArray = trailerVideo.split("")
+        console.log(linkArray, "after split")
+        // linkArray.splice(27, 20);
+        // console.log(linkArray, "after splice")
+        trailerVideo = linkArray.join("");
+        console.log(trailerVideo);
+        document.querySelector("#trailer").href = trailerVideo;
+        
+    })
+    
+    
+    fetch(`https://imdb-api.com/API/FullCast/${imdbKey}/${imdbId}`)
+    .then(response => response.json())
+    .then(function(castData){
+        let castList = castData.actors;
+        for (let i = 0; i < 10; i++) {
+            const actorList = castList[i];
+            console.log(actorList.name);
+            let actorName = actorList.name;
+            let castMember = document.createElement("li");
+            castMember.textContent = actorName;
+            castEl.append(castMember);
+        }
+    })
+    
+    fetch(`https://imdb-api.com/API/Reviews/${imdbKey}/${imdbId}/`)
+    .then(response => response.json())
+    .then(function(reviewData){
+        console.log(reviewData)
+        let reviewList = reviewData.items;
+        for (let i = 0; i < 2; i++) {
+            const itemsList = reviewList[i];
+            console.log(itemsList.content);
+            let reviewItems = itemsList.content;
+            let movieReview = document.createElement("li");
+            movieReview.textContent = reviewItems;
+            reviewEl.append(movieReview);
+            
+            
+            
+            
+        }
+        
+        searchBar.value = "";
+    })
+    
+}
 function renderBtn(){
     recentlySearched.innerHTML = "";
     for (let i = 0; i < movieList.length; i++) {
         const movie = movieList[i];
         let newSearchBtn = document.createElement("button");
+
         newSearchBtn.textContent = movie;
         recentlySearched.append(newSearchBtn);
         newSearchBtn.classList.add("searchHistoryBtn");
@@ -149,7 +151,11 @@ renderBtn();
 
 recentlySearched.addEventListener("click", function(event){
     if(event.target.matches("button")) {
-        getMovieData(event.target.textContent)
+        castEl.innerHTML = ""
+        streamingDataEl.innerHTML ="";
+        reviewEl.innerHTML = ""
+        getIMDB_ID_ByTitle(event.target.textContent)
+        
         
 
 
